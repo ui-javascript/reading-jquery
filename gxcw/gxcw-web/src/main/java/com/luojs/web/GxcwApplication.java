bf57b041c58825b30d7f9990cc3cb1f266c9b479
luo0412 <1095847440@qq.com>
2018-03-16 16:04:30 +0800

    调整文件结构 small

---
+++ gxcw/gxcw-web/src/main/java/com/luojs/web/GxcwApplication.java
@@ -0,0 +1,12 @@
+package com.luojs.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GxcwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GxcwApplication.class, args);
+	}
+}
