8cac504c53317789b4a11c616ea4113445942407
luo0412 <1095847440@qq.com>
2018-03-16 16:04:17 +0800

    调整文件结构 small

--- gxcw-parent/README.md
+++
@@ -1,3 +0,0 @@
-# README.md
-
-http://blog.csdn.net/u014695188/article/details/78241167
--- gxcw-parent/gxcw-entity/src/main/java/User.java
+++
@@ -1,8 +0,0 @@
-/**
- * Description:
- * User: luo0412 1095847440@qq.com
- * Date: 2018-03-13
- * Time: 17:30
- */
-public class User {
-}
