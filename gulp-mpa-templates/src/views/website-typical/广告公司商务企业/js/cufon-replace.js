e81bad08780bdc5d1b0b8153cef2725b24660e69
luo0412 <1095847440@qq.com>
2018-05-25 14:06:11 +0800

    adjust

--- gulp-mpa-templates/src/views/website-typical/广告公司商务企业/js/cufon-replace.js
+++
@@ -1,3 +0,0 @@
-Cufon.replace('h3, .list-2 .item', { fontFamily: 'NewsGoth', hover:true });
-Cufon.replace('.menu li a, h1 strong, .pagination strong, .banner strong', { fontFamily: 'NewsGoth bold', hover:true });
-Cufon.replace('h2', { fontFamily: 'Vegur', hover:true });
